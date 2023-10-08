05fc788fa0461708464310ae0e4bfd0f417a6d2f
Colin Evrard <colin.evrard.134@gmail.com>
2023-10-08 22:48:17 +0200

    Make the compile time debug settings more granular

--- src/cuistax/cui_debug.hxx
+++ src/cuistax/cui_debug.hxx
@@ -1,16 +1,33 @@
 #pragma once
 #include <stdio.h>
-#ifndef CUISTAX_DEBUG_LEVEL
-#define CUISTAX_DEBUG_LEVEL 4
+#ifndef CUISTAX_LOG_LEVEL
+#define CUISTAX_LOG_LEVEL 4
 #endif
-#ifdef CUISTAX_DISABLE_LOG
-#define CUISTAX_ERR(fmt_str, ...) ;
-#define CUISTAX_WARN(fmt_str, ...) ;
-#define CUISTAX_LOG(fmt_str, ...) ;
-#define CUISTAX_DEBUG(fmt_str, ...) ;
+#ifndef CUISTAX_LOG
+    #define CUISTAX_ERR(fmt_str, ...) do {} while(0)
+    #define CUISTAX_WARN(fmt_str, ...) do {} while(0)
+    #define CUISTAX_LOG(fmt_str, ...) do {} while(0)
+    #define CUISTAX_DEBUG(fmt_str, ...) do {} while(0)
 #else
-#define CUISTAX_ERR(fmt_str, ...) if ((CUISTAX_DEBUG_LEVEL > 0)) printf("[ERROR]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
-#define CUISTAX_WARN(fmt_str, ...) if ((CUISTAX_DEBUG_LEVEL > 1)) printf("[WARNING]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__ )
-#define CUISTAX_LOG(fmt_str, ...) if ((CUISTAX_DEBUG_LEVEL) > 2) printf("[LOG]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
-#define CUISTAX_DEBUG(fmt_str, ...) if ((CUISTAX_DEBUG_LEVEL > 3)) printf("[DEBUG]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
+    #if CUISTAX_LOG_LEVEL > 0
+        #define CUISTAX_ERR(fmt_str, ...)  printf("[ERROR]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
+    #else
+        #define CUISTAX_ERR(fmt_str, ...) do {} while(0)
+    #endif
+    #if CUISTAX_LOG_LEVEL > 1
+        #define CUISTAX_WARN(fmt_str, ...) printf("[WARNING]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__ )
+    #else
+        #define CUISTAX_WARN(fmt_str, ...) do {} while(0)
+    #endif
+    #if CUISTAX_LOG_LEVEL > 2
+        #define CUISTAX_LOG(fmt_str, ...) printf("[LOG]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
+    #else
+        #define CUISTAX_LOG(fmt_str, ...) do {} while(0)
+    #endif
+    #if CUISTAX_LOG_LEVEL > 3
+        #define CUISTAX_DEBUG(fmt_str, ...) printf("[DEBUG]: " fmt_str "\n" __VA_OPT__(,) __VA_ARGS__)
+    #else
+        #define CUISTAX_DEBUG(fmt_str, ...) do {} while(0)
+    #endif
+    
 #endif
